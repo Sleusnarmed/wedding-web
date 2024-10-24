b8e51fbb286b9a309dbe3a54c3c3b6f0eeaf1ebb
José Salazar <jmsalazar.macg@hotmail.com>
2024-10-23 20:33:22 -0600

    videoPlayer-implemented

--- src/app/lamejornochesari&dany/page.jsx
+++ src/app/lamejornochesari&dany/page.jsx
@@ -6,6 +6,7 @@ import styles from "./lamejornochesari&dany.module.css";
 import Countdown from "@/components/countdown/Countdown";
 import Carrier from "@/components/carrier/Carrier";
 import Confirmation from "@/components/confirmation/Confirmation";
+import VideoPlayer from "@/components/videoPlayer/VideoPlayer";
 
 const getData = async () => {
   const res = await fetch("/api/guests", {
@@ -67,98 +68,102 @@ const BodaDaraiPage = () => {
   }
 
   return (
-    <div className={styles.mainContainer}>
-      <div className={styles.pageContainer}>
-        <div className={styles.decortaionLightsContainer__topPage}>
-          <Image
-            src="/img/decoration-lights_mainPage.png"
-            alt="Decotración lamejornochesari y Dany"
-            width={300}
-            height={100}
-          />
-        </div>
-        <h1 className={styles.names}>Daniel y Saraí</h1>
-        <Countdown />
-        <div className={styles.gratitudeText}>
-          <p className={styles.title}>¡El amor esta en el aire!</p>
-          <p className={styles.paragraph}>
-            Desde que nos conocimos andábamos entre las nubes pero nunca
-            pensamos que al pasar el tiempo compartiríamos experiencias únicas,
-            pláticas con muchas risas y música que saldría de nuestros
-            corazones. <br /><br />Llegamos a descubrir que las cosas juntos eran más
-            divertidas y llenas de amor, sabemos ahora que es el momento ideal
-            para compartir esta nueva aventura de la mano de Dios; nos
-            encantaría celebrar este único momento con la compañía de nuestra
-            familia y mejores amigos.
-          </p>
-          <p className={styles.farewell}>¿Nos acompañarías?</p>
-        </div>
-        <div className={styles.decorationLightsContainer__midPage}>
-          <Image
-            src="/img/decoration-lights_mainPage.png"
-            alt="Decotración lamejornochesari y Dany"
-            width={300}
-            height={100}
-          />
-        </div>
-        <div className={styles.bibleVerse}>
-          <p className={styles.verse}>
-            “Las muchas aguas no podrán apagar el amor, ni lo ahogarán los ríos.
-            Si diese el hombre todos los bienes de su casa por este amor, de
-            cierto lo menospreciarían.”
-          </p>
-          <p className={styles.verseNumber}>Cantares 8:7</p>
-        </div>
-
-        {/* Componente de Confirmación */}
-        <Confirmation
-          guestId={data?.id}
-          initialConfirmation={data?.confirmation}
-          data={data}
-        />
-        <div className={styles.decorationLightsContainer__bottomPage}>
-          <Image
-            src="/img/decoration-lights_mainPage.png"
-            alt="Decotración lamejornochesari y Dany"
-            width={300}
-            height={100}
-          />
-        </div>
-        <div className={styles.dressCode}>
-          <h2>——Dress code——</h2>
-          <p> Vestimenta formal </p>
-        </div>
-
-        <div className={styles.forbiddenColors}>
-          <h2>— Colores prohibidos —</h2>
-          <div className={styles.forbiddenColors__ImageContainer}>
+    <>
+      
+      <div className={styles.mainContainer}>
+        <div className={styles.pageContainer}>
+        <VideoPlayer></VideoPlayer>
+          <div className={styles.decortaionLightsContainer__topPage}>
             <Image
-              src="/img/forbiddenColors.png"
-              alt="Colores prohibidos"
+              src="/img/decoration-lights_mainPage.png"
+              alt="Decotración lamejornochesari y Dany"
               width={300}
-              height={80}
+              height={100}
             />
           </div>
-        </div>
+          <h1 className={styles.names}>Daniel y Saraí</h1>
+          <Countdown />
+          <div className={styles.gratitudeText}>
+            <p className={styles.title}>¡El amor esta en el aire!</p>
+            <p className={styles.paragraph}>
+              Desde que nos conocimos andábamos entre las nubes pero nunca
+              pensamos que al pasar el tiempo compartiríamos experiencias únicas,
+              pláticas con muchas risas y música que saldría de nuestros
+              corazones. <br /><br />Llegamos a descubrir que las cosas juntos eran más
+              divertidas y llenas de amor, sabemos ahora que es el momento ideal
+              para compartir esta nueva aventura de la mano de Dios; nos
+              encantaría celebrar este único momento con la compañía de nuestra
+              familia y mejores amigos.
+            </p>
+            <p className={styles.farewell}>¿Nos acompañarías?</p>
+          </div>
+          <div className={styles.decorationLightsContainer__midPage}>
+            <Image
+              src="/img/decoration-lights_mainPage.png"
+              alt="Decotración lamejornochesari y Dany"
+              width={300}
+              height={100}
+            />
+          </div>
+          <div className={styles.bibleVerse}>
+            <p className={styles.verse}>
+              “Las muchas aguas no podrán apagar el amor, ni lo ahogarán los ríos.
+              Si diese el hombre todos los bienes de su casa por este amor, de
+              cierto lo menospreciarían.”
+            </p>
+            <p className={styles.verseNumber}>Cantares 8:7</p>
+          </div>
 
-        <div className={styles.giftTable}>
-          <h2 className={styles.giftTable__title}>
-            Si gustas regalarnos algo:{" "}
-          </h2>
-          <div className={styles.linkTablesContainer}>
-            <a href="/gift-table" className={styles.linkTable}>
-              Tabla de regalos
-            </a>
-            <a href="/registry" className={styles.linkTable}>
-              Registro
-            </a>
-            <a href="/lamejornochesari&dany/page" className={styles.linkTable}>
-              Página de la boda
-            </a>
+          {/* Componente de Confirmación */}
+          <Confirmation
+            guestId={data?.id}
+            initialConfirmation={data?.confirmation}
+            data={data}
+          />
+          <div className={styles.decorationLightsContainer__bottomPage}>
+            <Image
+              src="/img/decoration-lights_mainPage.png"
+              alt="Decotración lamejornochesari y Dany"
+              width={300}
+              height={100}
+            />
+          </div>
+          <div className={styles.dressCode}>
+            <h2>——Dress code——</h2>
+            <p> Vestimenta formal </p>
+          </div>
+
+          <div className={styles.forbiddenColors}>
+            <h2>— Colores prohibidos —</h2>
+            <div className={styles.forbiddenColors__ImageContainer}>
+              <Image
+                src="/img/forbiddenColors.png"
+                alt="Colores prohibidos"
+                width={300}
+                height={80}
+              />
+            </div>
+          </div>
+
+          <div className={styles.giftTable}>
+            <h2 className={styles.giftTable__title}>
+              Si gustas regalarnos algo:{" "}
+            </h2>
+            <div className={styles.linkTablesContainer}>
+              <a href="/gift-table" className={styles.linkTable}>
+                Tabla de regalos
+              </a>
+              <a href="/registry" className={styles.linkTable}>
+                Registro
+              </a>
+              <a href="/lamejornochesari&dany/page" className={styles.linkTable}>
+                Página de la boda
+              </a>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
---
+++ src/components/videoPlayer/VideoPlayer.jsx
@@ -0,0 +1,28 @@
+// src/components/VideoPlayer.jsx
+import { useState } from 'react';
+import { YouTubeEmbed } from '@next/third-parties/google'
+import styles from './videoPlayer.module.css';
+
+export default function VideoPlayer() {
+    const [isVisible, setIsVisible] = useState(true);
+
+    const handleClose = () => {
+        setIsVisible(false);
+        window.scrollTo(0, 0);
+    };
+
+    return (
+        <>  
+            {isVisible && (
+                <div className={styles.mainVideoContainer}>
+                    <div className={styles.videoContainer}>
+                        <YouTubeEmbed videoId="qbrSuaxnMS0" width={700} height={250} params="autoplay=1" frameborder="0" allow="autoplay; fullscreen" />
+                        <button onClick={handleClose} className={styles.closeButton}>
+                            &times;
+                        </button>
+                    </div>
+                </div>
+            )}
+        </>
+    );
+}
