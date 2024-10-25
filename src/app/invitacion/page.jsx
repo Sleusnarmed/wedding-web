c235ed80c5da44aeb7bd99313d5859f6fa310604
José Salazar <jmsalazar.macg@hotmail.com>
2024-10-24 22:12:42 -0600

    dif-height

--- src/app/invitacion/page.jsx
+++ src/app/invitacion/page.jsx
@@ -142,7 +142,7 @@ const BodaDaraiPage = () => {
                 src="/img/forbiddenColors.png"
                 alt="Colores prohibidos"
                 width={300}
-                height={55}
+                height={45}
               />
             </div>
           </div>
