a528115d7ef0c55e4f22841e374d96ff6a2ee51b
José Salazar <jmsalazar.macg@hotmail.com>
2024-11-01 13:53:43 -0600

    ui/ux-improvement

--- src/components/confirmation/Confirmation.jsx
+++ src/components/confirmation/Confirmation.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useCallback } from "react";
-import styles from './confirmation.module.css';
+import styles from "./confirmation.module.css";
 
 const Confirmation = ({ guestId, initialConfirmation, data, onUpdate }) => {
   const [confirmation, setConfirmation] = useState(initialConfirmation);
@@ -7,32 +7,37 @@ const Confirmation = ({ guestId, initialConfirmation, data, onUpdate }) => {
   const [error, setError] = useState("");
   const [guestsConfirmed, setGuestsConfirmed] = useState(1); // Empieza en 1 como mínimo
 
-  const handleConfirmation = useCallback(async (confirm) => {
-    setLoading(true);
-    setError("");
+  const handleConfirmation = useCallback(
+    async (confirm) => {
+      setLoading(true);
+      setError("");
 
-    try {
-      const body = JSON.stringify({ confirmation: confirm, guestsConfirmed }); // Asegúrate de que guestsConfirmed se envía aquí
-      const res = await fetch(`/api/guests/${guestId}`, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: body,
-      });
+      try {
+        const body = JSON.stringify({ confirmation: confirm, guestsConfirmed }); // Asegúrate de que guestsConfirmed se envía aquí
+        const res = await fetch(`/api/guests/${guestId}`, {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: body,
+        });
 
-      if (!res.ok) {
-        throw new Error("Error al confirmar asistencia. Inténtalo nuevamente.");
+        if (!res.ok) {
+          throw new Error(
+            "Error al confirmar asistencia. Inténtalo nuevamente."
+          );
+        }
+
+        setConfirmation(confirm);
+        onUpdate(guestsConfirmed); // Pasa guestsConfirmed a onUpdate para que se actualice en el componente principal
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
       }
-
-      setConfirmation(confirm);
-      onUpdate(guestsConfirmed); // Pasa guestsConfirmed a onUpdate para que se actualice en el componente principal
-    } catch (error) {
-      setError(error.message);
-    } finally {
-      setLoading(false);
-    }
-  }, [guestId, guestsConfirmed, onUpdate]);
+    },
+    [guestId, guestsConfirmed, onUpdate]
+  );
 
   const handleGuestChange = useCallback((e) => {
     const value = parseInt(e.target.value, 10);
@@ -43,7 +48,11 @@ const Confirmation = ({ guestId, initialConfirmation, data, onUpdate }) => {
     return (
       <div className={styles.confirmedContainer}>
         <h1>Asistencia confirmada</h1>
-        <button onClick={() => handleConfirmation(false)} disabled={loading} className={styles.cancelConfirmationButton}>
+        <button
+          onClick={() => handleConfirmation(false)}
+          disabled={loading}
+          className={styles.cancelConfirmationButton}
+        >
           Cancelar asistencia
         </button>
         {loading && <p></p>}
@@ -54,8 +63,11 @@ const Confirmation = ({ guestId, initialConfirmation, data, onUpdate }) => {
   return (
     <div className={styles.confirmationContainer}>
       <h1>Confirmar tu asistencia</h1>
-      <p className={styles.invAvalaible}>¡Hola {data.name}! <br />Tienes {data.maxinvguests} invitados</p>
-      <select
+      <p className={styles.invAvalaible}>
+        ¡Hola {data.name}! <br />
+        {data.maxinvguests === 1 ? "Estás invitado" : `Puedes asistir con ${data.maxinvguests - 1} acompañante(s)`}
+      </p>
+      {data.maxinvguests === 1 ? <p>Solamente tú</p> : <><select
         id="guestsConfirmed"
         name="guestsConfirmed"
         className={styles.customSelect}
@@ -65,12 +77,19 @@ const Confirmation = ({ guestId, initialConfirmation, data, onUpdate }) => {
       >
         {/* Genera las opciones según el número máximo de invitados */}
         {[...Array(data.maxinvguests)].map((_, index) => (
-          <option key={index + 1} value={index + 1}>
+          <option key={index + 1} value={index}>
             {index + 1}
           </option>
         ))}
       </select>
-      <button onClick={() => handleConfirmation(true)} disabled={loading || guestsConfirmed < 1} className={styles.confirmationButton}>
+      <p className={styles.notice}>El número seleccionado incluye a ti y tus acompañantes.</p>
+      </>
+      }
+      <button
+        onClick={() => handleConfirmation(true)}
+        disabled={loading || guestsConfirmed < 1}
+        className={styles.confirmationButton}
+      >
         Confirmar asistencia
       </button>
       {error && <p className={styles.errorText}>{error}</p>}
