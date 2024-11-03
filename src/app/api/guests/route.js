9c89269634fc56a0cd1cdd21787483a329b1f9b8
José Salazar <jmsalazar.macg@hotmail.com>
2024-11-03 00:00:24 -0600

    dynamicbug

--- src/app/api/guests/route.js
+++ src/app/api/guests/route.js
@@ -3,6 +3,7 @@ import { sql } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
 
 // Manejo de solicitud GET
+export const dynamic = "force-dynamic";
 export async function GET(request) {
   try {
     const { rows } = await sql`SELECT * FROM guests`;
--- src/components/confirmation/Confirmation.jsx
+++ src/components/confirmation/Confirmation.jsx
@@ -13,7 +13,7 @@ const Confirmation = ({ guestId, initialConfirmation, data, onUpdate }) => {
       setError("");
 
       try {
-        const body = JSON.stringify({ confirmation: confirm, guestsConfirmed }); // Asegúrate de que guestsConfirmed se envía aquí
+        const body = JSON.stringify({ confirmation: confirm, guestsConfirmed });
         const res = await fetch(`/api/guests/${guestId}`, {
           method: "PUT",
           headers: {
@@ -29,7 +29,7 @@ const Confirmation = ({ guestId, initialConfirmation, data, onUpdate }) => {
         }
 
         setConfirmation(confirm);
-        onUpdate(guestsConfirmed); // Pasa guestsConfirmed a onUpdate para que se actualice en el componente principal
+        onUpdate(guestsConfirmed);
       } catch (error) {
         setError(error.message);
       } finally {
@@ -55,7 +55,7 @@ const Confirmation = ({ guestId, initialConfirmation, data, onUpdate }) => {
         >
           Cancelar asistencia
         </button>
-        {loading && <p></p>}
+        {loading && <p className={styles.loadingText}>Procesando...</p>}
       </div>
     );
   }
@@ -65,35 +65,47 @@ const Confirmation = ({ guestId, initialConfirmation, data, onUpdate }) => {
       <h1>Confirmar tu asistencia</h1>
       <p className={styles.invAvalaible}>
         ¡Hola {data.name}! <br />
-        {data.maxinvguests === 1 ? <p></p> : `Puedes asistir con ${data.maxinvguests - 1} acompañante(s)`}
+        {data.maxinvguests === 1 ? null : `Puedes asistir con ${data.maxinvguests - 1} acompañante(s)`}
       </p>
-      {data.maxinvguests === 1 ? <p></p> : <><select
-        id="guestsConfirmed"
-        name="guestsConfirmed"
-        className={styles.customSelect}
-        value={guestsConfirmed}
-        onChange={handleGuestChange}
-        disabled={loading}
-      >
-        {/* Genera las opciones según el número máximo de invitados */}
-        {[...Array(data.maxinvguests)].map((_, index) => (
-          <option key={index + 1} value={index}>
-            {index + 1}
-          </option>
-        ))}
-      </select>
-      <p className={styles.notice}>El número seleccionado incluye a ti y tus acompañantes.</p>
-      </>
-      }
-      <button
-        onClick={() => handleConfirmation(true)}
-        disabled={loading || guestsConfirmed < 1}
-        className={styles.confirmationButton}
-      >
-        Confirmar asistencia
-      </button>
-      {error && <p className={styles.errorText}>{error}</p>}
-      {loading && <p></p>}
+      {data.maxinvguests === 1 ? (
+        <button
+          onClick={() => handleConfirmation(true)}
+          disabled={loading || guestsConfirmed < 1}
+          className={styles.confirmationButton}
+        >
+          Confirmar asistencia
+        </button>
+      ) : (
+        <>
+          <select
+            id="guestsConfirmed"
+            name="guestsConfirmed"
+            className={styles.customSelect}
+            value={guestsConfirmed}
+            onChange={handleGuestChange}
+            disabled={loading}
+          >
+            {/* Genera las opciones desde 1 hasta el número máximo de invitados */}
+            {[...Array(data.maxinvguests)].map((_, index) => (
+              <option key={index + 1} value={index + 1}>
+                {index + 1}
+              </option>
+            ))}
+          </select>
+          <p className={styles.notice}>
+            El número seleccionado incluye a ti y tus acompañantes.
+          </p>
+          <button
+            onClick={() => handleConfirmation(true)}
+            disabled={loading || guestsConfirmed < 1}
+            className={styles.confirmationButton}
+          >
+            Confirmar asistencia
+          </button>
+          {error && error.length > 0 && <p className={styles.errorText}>{error}</p>}
+          {loading && <p className={styles.loadingText}>Procesando...</p>}
+        </>
+      )}
     </div>
   );
 };
