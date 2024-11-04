8e577297bd0f3266c183afc88d20940fb8c2c4b2
José Salazar <jmsalazar.macg@hotmail.com>
2024-11-03 23:36:51 -0600

    Cache-control

--- src/app/api/guests/route.js
+++ src/app/api/guests/route.js
@@ -2,13 +2,16 @@
 import { sql } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
 export const dynamic = 'force-dynamic'
+
 export async function GET(request) {
   try {
     const { rows } = await sql`SELECT * FROM guests`;
     return new NextResponse(JSON.stringify(rows), {
       status: 200,
       headers: {
-        'Cache-Control': 'no-store',  // Desactiva la caché
+        'Cache-Control': 'no-store, must-revalidate',  // Desactiva la caché y obliga a revalidar
+        'Pragma': 'no-cache',
+        'Expires': '0',
       },
     });
   } catch (error) {
